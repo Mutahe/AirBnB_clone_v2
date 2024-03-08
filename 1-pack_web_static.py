231ab59faffc34cdcac0d358c9c42974fecc2f64
isiji <blairisiji@gmail.com>
2024-03-08 01:12:08 -0800

    fabri correction

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,16 +4,24 @@
 from datetime import datetime
 from fabric.api import *
 import os
-import fabric
 
 @task
 def do_pack():
     time = datetime.now()
     archive = 'web_static_' + time.strftime("%Y%m%d%H%M%S") + '.' + 'tgz'
-    local('sudo mkdir -p versions')
+
+    with hide('running'):
+        local('sudo mkdir -p versions')
+
+    print(f"Packing web_static to versions/{archive}")
+
     filemade = local('tar -cvzf versions/{} web_static'.format(archive))
+
+    file_size =  os.path.getsize(f"versions/{archive}")
+
+    print(f"web_static packed: {archive} -> {file_size}Bytes")
+
     
     if filemade is not None:
         return archive
-    else:
-        return None
+
